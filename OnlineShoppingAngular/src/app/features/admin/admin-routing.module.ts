972a16c52a6fbbb92dcf0449f417bfdcebe8a77a
CashZzz2002 <nyinyi0232@gmail.com>
2025-06-14 15:28:02 +0630

    nnhl1

--- OnlineShoppingAngular/src/app/features/admin/admin-routing.module.ts
+++ OnlineShoppingAngular/src/app/features/admin/admin-routing.module.ts
@@ -49,7 +49,7 @@ const routes: Routes = [
   },
   {
     path: 'bulkUploadProduct',
-    component: ProductBulkUploadComponent, canActivate: [NoAuthGuard],
+    component: ProductBulkUploadComponent,
   },
 
 ];
